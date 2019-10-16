725f65646786cf9d9ec457122eacbfe1272c609d
pyrocoder <pyro@bk.ru>
2019-10-16 17:13:37 +0300

    as

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        adaptiveHeight: true,
+        adaptiveHeight: false,
         prevArrow: '<button type="button" class="arrows__left"></button>',
         nextArrow: '<button type="button" class="arrows__right"></button>',
         responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false
-                }
-            }
+            // {
+            //     breakpoint: 992,
+            //     settings: {
+            //         dots: true,
+            //         arrows: false
+            //     }
+            // }
         ]
     });
 });
